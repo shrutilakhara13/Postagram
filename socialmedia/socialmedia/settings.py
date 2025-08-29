8626b21f93219b297c6cebdec7e162023191f847
Shruti Lakhara <118791364+shrutilakhara13@users.noreply.github.com>
2025-08-29 16:36:28 +0530

    updated allowed hosts

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(" ")
+
+ALLOWED_HOSTS = ["postagram-h1vu.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
